b7b603ad1096008515dff131a0a06f89c04b8ee1
Roland Rybka <roland.rybka@gmail.com>
2023-07-27 08:32:36 +0200

    Initial commit v2

--- sensor.py
+++ sensor.py
@@ -1,140 +1,82 @@
 import logging
-import pkg_resources
-import yaml
-from datetime import timedelta
-from homeassistant import config_entries
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import CONF_NAME
 from homeassistant.helpers.entity import Entity
-from pymodbus.client import ModbusTcpClient as ModbusClient
-from .config_flow import AerauliqaModbusFlowHandler  # Import the Config Flow Handler
-from .const import DOMAIN
+from pymodbus.client import ModbusTcpClient
+from pymodbus.exceptions import ConnectionException
 
 logging.basicConfig(level=logging.DEBUG)
 _LOGGER = logging.getLogger(__name__)
 
-CONF_HOST = "host"
-CONF_PORT = "port"
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_NAME): cv.string,
+    vol.Required('host'): str,
+    vol.Required('port'): int,
+    vol.Required('sensors'): vol.All(),
+})
 
-CONF_NAME = "name"
-CONF_SLAVE = "slave"
-CONF_ADDRESS = "address"
-CONF_INPUT_TYPE = "input_type"
-CONF_DATA_TYPE = "data_type"
-CONF_UNIT_OF_MEASUREMENT = "unit_of_measurement"
-CONF_COUNT = "count"
-CONF_VALUE_MAP = "value_map"
-CONF_WRITABLE = "writable"
-CONF_SCALE = "scale"
-
-INPUT_TYPE_HOLDING = "holding"
-INPUT_TYPE_INPUT = "input"
-
-DATA_TYPE_UINT32 = "uint32"
-DATA_TYPE_UINT16 = "uint16"
-DATA_TYPE_FLOAT = "float"
-
-async def async_setup(hass, config):
-    """Set up the Aerauliqa Modbus integration."""
-    # Check if the configuration is provided via the Home Assistant UI (config flow)
-    if DOMAIN in config:
-        # Perform any initial setup if needed
-        return True
-
-    return True
-
-
-async def async_setup_entry(hass, entry, async_add_entities):
-    config = entry.data
-    client = ModbusClient(host=config[CONF_HOST], port=config[CONF_PORT], timeout=10)
-
-    if not client.connect():
-        _LOGGER.error("::Modbus Unable to connect to Modbus device at %s:%s", config[CONF_HOST], config[CONF_PORT])
+async def async_setup_platform(hass, config, add_entities, discovery_info=None):
+    name = config[CONF_NAME]
+    host = config['host']
+    port = config['port']
+    sensors = config['sensors']
+    try:
+        client = ModbusTcpClient(host, port)
+        client.connect()
+        _LOGGER.debug("::Modbus connection OK! %s:%s", host, port)
+    except ConnectionException as ex:
+        _LOGGER.error("::Error connecting to Modbus device: %s", str(ex))
         return
 
-    coordinator = DataUpdateCoordinator(
-        hass,
-        _LOGGER,
-        name="aerauliqa_modbus",
-        update_method=update_data,
-        update_interval=timedelta(seconds=30),
-        client=client,
-    )
+    modbus_sensors = []
+    for sensor in sensors:
+        modbus_sensor = AerauliqaQRSensor(client, sensor)
+        modbus_sensors.append(modbus_sensor)
+        if modbus_sensor._input_type == 'input':
+            _LOGGER.debug("::Modbus add write service %s", modbus_sensor._name)
+            async def handle_write_service(call):
+                state = call.data.get("state")
+                if state is not None:
+                    await modbus_sensor.async_set_state(state)
+            hass.services.async_register("aerauliqa_qr", modbus_sensor.service_name + "_write", handle_write_service)
 
-    await coordinator.async_config_entry_first_refresh()
+    add_entities(modbus_sensors)
 
-    sensors = []
-    with pkg_resources.resource_stream(__name__, "sensors.yaml") as f:
-        sensor_configs = yaml.safe_load(f)
-        for sensor_config in sensor_configs:
-            sensors.append(AerauliqaModbusSensor(coordinator, sensor_config))
-
-    async_add_entities(sensors)
-
-async def update_data(client, config):
-    try:
-        response = client.read_holding_registers(
-            address=config[CONF_ADDRESS],
-            count=config[CONF_COUNT],
-            unit=config[CONF_SLAVE],
-        )
-
-        if response.isError():
-            raise UpdateFailed("Modbus error response")
-
-        data = response.registers
-
-        # Calculate the value from the data (implement based on your data type)
-
-        # For example, if the data type is UINT16 (two bytes), combine the two registers:
-        if config[CONF_DATA_TYPE] == DATA_TYPE_UINT16:
-            numeric_value = data[0]
-
-        # For example, if the data type is UINT32 (four bytes), combine four registers:
-        elif config[CONF_DATA_TYPE] == DATA_TYPE_UINT32:
-            numeric_value = (data[0] << 16) | data[1]
-
-        # For example, if the data type is FLOAT (four bytes), combine four registers:
-        elif config[CONF_DATA_TYPE] == DATA_TYPE_FLOAT:
-            numeric_value = (data[0] << 24) | (data[1] << 16) | (data[2] << 8) | data[3]
-
-        # Add other data types as needed...
-
-        # Convert numeric value to meaningful state using the value_map
-        if CONF_VALUE_MAP in config:
-            value_map = config[CONF_VALUE_MAP]
-            if numeric_value in value_map:
-                state = value_map[numeric_value]
-            else:
-                state = numeric_value
-        else:
-            state = numeric_value
-
-        return state
-
-    except Exception as ex:
-        _LOGGER.error("Error updating data: %s", str(ex))
-        raise UpdateFailed("Error updating data")
-
-class AerauliqaModbusSensor(Entity):
-    def __init__(self, coordinator, config):
-        self._coordinator = coordinator
-        self._client = coordinator.client
-        self._name = config[CONF_NAME]
-        self._slave = config[CONF_SLAVE]
-        self._address = config[CONF_ADDRESS]
-        self._input_type = config[CONF_INPUT_TYPE]
-        self._data_type = config[CONF_DATA_TYPE]
-        self._unit_of_measurement = config.get(CONF_UNIT_OF_MEASUREMENT, None)
-        self._scale = config.get(CONF_SCALE, None)
-        self._count = config[CONF_COUNT]
+class AerauliqaQRSensor(Entity):
+    def __init__(self, client, config):
+        self.entity_id = config.get('entity_id', None);
+        self.service_name = config.get('service_name', None);
+        self._client = client
+        self._name = config['name']
+        self._slave = config['slave']
+        self._address = config['address']
+        self._input_type = config['input_type']
+        self._data_type = config['data_type']
+        self._unit_of_measurement = config.get('unit_of_measurement', None)
+        self._scale = config.get('scale', None)
+        self._count = config['count']
         self._state = None
         self._value_map = config.get('value_map', {})  # Mapping of numeric values to states
-        self._writable = self._input_type == INPUT_TYPE_INPUT
 
     @property
     def name(self):
         return self._name
 
+    @property
+    def unique_id(self):
+        return 'uq_' + self.entity_id
+
+    @property
+    def supported_features(self):
+        """Flag supported features."""
+        if self._input_type == "input":
+            _LOGGER.debug("::Modbus register %s is input", self._name)
+            return 1
+        _LOGGER.debug("::Modbus register %s is read only", self._name)
+        return 0
+    
     @property
     def state(self):
         if self._state is not None:
@@ -145,131 +87,62 @@ class AerauliqaModbusSensor(Entity):
     def unit_of_measurement(self):
         return self._unit_of_measurement
 
-    @property
-    def is_on(self):
-        return self._state
-
-    @property
-    def should_poll(self):
-        return True
-
-    # def update(self):
-    #     try:
-    #         _LOGGER.debug("::Modbus read holding reg, address: %s, slave: %s", self._address, self._slave)
-    #         response = self._client.read_holding_registers(
-    #             self._address,
-    #             count=self._count,
-    #             slave=self._slave
-    #         )
-
-    #         if response.isError():
-    #             _LOGGER.error("::Modbus error response: %s", str(response))
-    #             # self._state = None
-    #         else:
-    #             data = response.registers  # Read all registers
-    #             if self._count == 1:
-    #                 numeric_value = data[0]
-    #             else:
-    #                 # Combine the values from both registers
-    #                 numeric_value = (data[0] << 16) | data[1]
-    #             if numeric_value in self._value_map:
-    #                 self._state = self._value_map[numeric_value]
-    #             else:
-    #                 if self._scale != None:
-    #                     self._state = numeric_value * self._scale
-    #                 else:
-    #                     self._state = numeric_value
-    #     except ConnectionException as ex:
-    #         _LOGGER.error("::Modbus connection error: %s", str(ex))
-    #         # self._state = None
-    # def write_register(self, value):
-    #     # Reverse the value_map to obtain numeric value from the selected state
-    #     numeric_value = next((k for k, v in self._value_map.items() if v == value), value)
-
-    #     try:
-    #         response = self._client.write_register(
-    #             address=self._address,
-    #             value=numeric_value,
-    #             slave=self._slave
-    #         )
-
-    #         if response.isError():
-    #             _LOGGER.error("Modbus error response: %s", str(response))
-    #         else:
-    #             _LOGGER.debug("Successfully wrote register: %s", self._address)
-    #     except ConnectionException as ex:
-    #         _LOGGER.error("Modbus connection error: %s", str(ex))            
-
-    async def async_update(self,config):
+    def update(self):
         try:
-            # Implement the appropriate Modbus read operation based on _input_type and _count
-            if self._input_type == INPUT_TYPE_HOLDING:
-                response = self._client.read_holding_registers(
-                    self._address, count=self._count, unit=self._slave
-                )
+            _LOGGER.debug("::Modbus read holding reg, address: %s, slave: %s", self._address, self._slave)
+            response = self._client.read_holding_registers(
+                self._address,
+                count=self._count,
+                slave=self._slave
+            )
+
+            if response.isError():
+                _LOGGER.error("::Modbus error response: %s", str(response))
+                # self._state = None
             else:
-                response = self._client.read_input_registers(
-                    self._address, count=self._count, unit=self._slave
-                )
+                data = response.registers  # Read all registers
+                if self._count == 1:
+                    numeric_value = data[0]
+                else:
+                    # Combine the values from both registers
+                    numeric_value = (data[0] << 16) | data[1]
+                if numeric_value in self._value_map:
+                    self._state = self._value_map[numeric_value]
+                else:
+                    if self._scale != None:
+                        self._state = numeric_value * self._scale
+                    else:
+                        self._state = numeric_value
+        except ConnectionException as ex:
+            _LOGGER.error("::Modbus connection error: %s", str(ex))
+            # self._state = None
+    def write_register(self, value):
+        # Reverse the value_map to obtain numeric value from the selected state
+        numeric_value = next((k for k, v in self._value_map.items() if v == value), value)
+        _LOGGER.error("::Modbus write register of: %s to %s", self._name, numeric_value)
+
+        try:
+            response = self._client.write_register(
+                address=self._address,
+                value=numeric_value,
+                slave=self._slave
+            )
 
             if response.isError():
                 _LOGGER.error("Modbus error response: %s", str(response))
-                raise UpdateFailed("Modbus error response")
-
-            data = response.registers
-            if self._count == 1:
-                numeric_value = data[0]
             else:
-                numeric_value = (data[0] << 16) | data[1]
+                _LOGGER.debug("Successfully wrote register: %s", self._address)
+        except ConnectionException as ex:
+            _LOGGER.error("Modbus connection error: %s", str(ex))            
 
-            # Convert numeric value to meaningful state using the value_map
-            if CONF_VALUE_MAP in config:
-                value_map = config[CONF_VALUE_MAP]
-                if numeric_value in value_map:
-                    self._state = value_map[numeric_value]
-                else:
-                    self._state = numeric_value
-            else:
-                self._state = numeric_value
-
-        except Exception as ex:
-            _LOGGER.error("Error updating data: %s", str(ex))
-            raise UpdateFailed("Error updating data")
-
-    async def async_turn_on(self, **kwargs):
-        # Implement the write operation for writable registers, if applicable
-        if self._writable:
-            try:
-                # Implement the appropriate Modbus write operation based on _input_type and _count
-                # For example, to write a value of 1 to a holding register at address 10:
-                response = self._client.write_register(10, 1, unit=self._slave)
-
-                if response.isError():
-                    _LOGGER.error("Modbus error response: %s", str(response))
-                    return
-
-                # Update the state after writing
-                self._state = 1
-
-            except Exception as ex:
-                _LOGGER.error("Error writing data: %s", str(ex))
-
-    async def async_turn_off(self, **kwargs):
-        # Implement the write operation for writable registers, if applicable
-        if self._writable:
-            try:
-                # Implement the appropriate Modbus write operation based on _input_type and _count
-                # For example, to write a value of 0 to a holding register at address 10:
-                response = self._client.write_register(10, 0, unit=self._slave)
-
-                if response.isError():
-                    _LOGGER.error("Modbus error response: %s", str(response))
-                    return
-
-                # Update the state after writing
-                self._state = 0
-
-            except Exception as ex:
-                _LOGGER.error("Error writing data: %s", str(ex))
-
-config_entries.HANDLERS.register(DOMAIN, AerauliqaModbusFlowHandler, priority=1)
+    async def async_set_state(self, state):
+        _LOGGER.error("::Modbus set state of: %s to %s", self._name, state)
+        if self._input_type == "input":
+            # Call the write_register method to write the value
+            self.write_register(state)
+            # Update the internal state to reflect the change
+            self._state = state
+            # Notify Home Assistant that the state has changed
+            self.async_write_ha_state()
+        else:
+            _LOGGER.warning("This entity is read-only and cannot be modified.")
